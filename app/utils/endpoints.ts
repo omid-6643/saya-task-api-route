7ba057c73390e02ebabc22956f89e4c9f956ae9b
omid-6643 <o.beygi@gmail.com>
2024-09-13 19:00:15 +0330

    fix endpoits

--- app/utils/endpoints.ts
+++ app/utils/endpoints.ts
@@ -1,9 +1,6 @@
-
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 const ENDPOINTS = {
-  post: `${baseURL}/api/v1/posts`,
-  randomPost: `${baseURL}/api/v1/posts/random`,
+  post: "/api/v1/posts",
+  randomPost: "/api/v1/posts/random",
 };
 
 export default ENDPOINTS;
